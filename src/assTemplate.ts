041487a93cd568a703052b867f41e7cee95754cc
Guillaume Lebigot <axel@teri-chan.net>
2019-08-23 21:50:34 +0200

    Fix \k90 not being good for a 1000ms delay

--- src/assTemplate.ts
+++ src/assTemplate.ts
@@ -127,7 +127,7 @@ export const dialogue = {
 	}
 };
 
-export const dialogueScript = '{\\fad(90,20)\\k90}';
-export const commentScript = '{\\k90}';
+export const dialogueScript = '{\\fad(90,20)\\k100}';
+export const commentScript = '{\\k100}';
 export const scriptFX = 'template pre-line all keeptags';
 export const script = '!retime("line",$start < 0 and -$start or -0,200)!{!$start < 0 and "\\\\k" .. ($start/10) or "\\\\k0"!\\fad(!$start < 0 and $start or 300!,200)}';
