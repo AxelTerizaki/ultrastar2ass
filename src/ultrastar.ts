543db2d12cecf265d3fd3bbd63a8ab5b29088a2a
Aeden <florentius.b@gmail.com>
2021-05-21 21:30:47 +0200

    :bug: Fix New line mark when start and end are different

--- src/ultrastar.ts
+++ src/ultrastar.ts
@@ -230,10 +230,10 @@ export default class UltrastarParser {
 				// Save the start time of the next sentence if it's present on the line, with absolute or relative beats
 				if (matches[1]) {
 					if (!relative) {
-						currentStart = Math.floor((this.config.offset ? this.config.offset : 0) + parseInt(matches[1]) * beatDuration);
+						currentStart = Math.floor((this.config.offset ? this.config.offset : 0) + this.meta.gap + parseInt(matches[1]) * beatDuration);
 						beatsCount = parseInt(matches[1]);
 					} else {
-						currentStart = Math.floor((this.config.offset ? this.config.offset : 0) + (beatsCount + parseInt(matches[1])) * beatDuration);
+						currentStart = Math.floor((this.config.offset ? this.config.offset : 0) + this.meta.gap + (beatsCount + parseInt(matches[1])) * beatDuration);
 						beatsCount += parseInt(matches[1]);
 					}
 				} // now, we *MAY* have computed the first offset : we do not need to keep this, ever
